a702bf17654d66148619294a6760e2bd7afea905
benja <benja@maletin.(none)>
2012-11-13 22:53:34 +0100

    add liquid-removal, break-obsidian, drops

--- src/main/java/com/yermocraft/Gipsy/ErCommandExecutor.java
+++ src/main/java/com/yermocraft/Gipsy/ErCommandExecutor.java
@@ -15,8 +15,11 @@ public class ErCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String comandName,
 			String[] args) {
-		if (args.length == 1 && args[0].equals("regen")) {
-			sender.sendMessage("Regenerated " + taskList.runAllPending() + " explosions");
+		if (sender.isOp()) {
+			if (args.length == 1 && args[0].equals("regen")) {
+				sender.sendMessage("Regenerated " + taskList.runAllPending() + " explosions");
+				return true;
+			}
 		}
 		return false;
 	}
---
+++ src/main/java/com/yermocraft/Gipsy/ExplosionRegenEffect.java
@@ -0,0 +1,23 @@
+package com.yermocraft.Gipsy;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class ExplosionRegenEffect implements Runnable {
+
+	private World world;
+	private Location location;
+
+	public ExplosionRegenEffect(World world, Location location) {
+		this.world = world;
+		this.location = location.add(0, 2, 0);
+	}
+
+	@Override
+	public void run() {
+		world.playEffect(location, Effect.ENDER_SIGNAL, 5);
+		world.playEffect(location, Effect.ZOMBIE_DESTROY_DOOR, 1);
+	}
+
+}
